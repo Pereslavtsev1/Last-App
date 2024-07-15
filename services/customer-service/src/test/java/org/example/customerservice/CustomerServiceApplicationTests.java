eb6cf1d3038ee76dd2d91550df49482ce04c6d2b
Vova Pereslavtsev <vovapereslavtsev@gmail.com>
2024-07-15 19:57:40 +0300

    added customer service

--- services/customer-service/src/test/java/org/example/customerservice/CustomerServiceApplicationTests.java
+++ services/customer-service/src/test/java/org/example/customerservice/CustomerServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.productservice;
+package org.example.customerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductServiceApplicationTests {
+class CustomerServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
